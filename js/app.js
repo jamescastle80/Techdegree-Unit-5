5757af5dc717353583dd40dbb3a3f85964fd5d88
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2020-08-10 17:45:33 +0200

    starter files

---
+++ js/app.js
@@ -0,0 +1,11 @@
+baguetteBox.run('.gallery', {
+    captions: true,
+    buttons: 'auto',
+    fullScreen: false
+});
+
+function myFunction() {
+    let input = document.getElementById('searchImput');
+    console.log(input.toUpperCase);
+}
+
