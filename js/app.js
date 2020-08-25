252484498de4769f2545f7995051afd43119f22a
James Castle <jamescastle@Jamess-MacBook-Pro.local>
2020-08-25 11:36:36 +0200

    search script

--- js/app.js
+++ js/app.js
@@ -4,8 +4,26 @@ baguetteBox.run('.gallery', {
     fullScreen: false
 });
 
-function myFunction() {
-    let input = document.getElementById('searchImput');
-    console.log(input.toUpperCase);
-}
+
+    
+    const searchInput = document.getElementById('search');
+    //console.log(searchInput);
+
+    searchInput.addEventListener('keyup', (e) => {
+        //console.log(e.target.value);
+        const searchString = e.target.value.toUpperCase();
+        console.log(searchString);
+        const slides = document.querySelectorAll(".slide a");
+        // loop through photos
+        for (let i = 0; i < gallery; i++) {
+            const caption = slide[i].getAttribute('data-caption').toUpperCase();
+
+            if (caption.includes(searchString)) {
+                //show image
+                $(slide[i]).show();
+            }
+                //hide image
+            else $(slide[i]).hide();
+        }
+    });
 
